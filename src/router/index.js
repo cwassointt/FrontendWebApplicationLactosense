4a9cdabd68e8818d31d87152e52601e1ed00b9e5
cwassointt <dillpixel1@gmail.com>
2024-10-29 21:14:39 -0500

    feat: added new testing routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/home.vue';
+import Dashboard from "../components/dashboard.vue";
+import Reports from "../components/reports.vue";
+import Alerts from "../components/alerts.vue";
+import Settings from "../components/settings.vue";
 
 const routes = [
-    { path: '/', component: Home }
+    { path: '/', component: Home },
+    { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+    { path: '/reports', name: 'Reports', component: Reports },
+    { path: '/alerts', name: 'Alerts', component: Alerts },
+    { path: '/settings', name: 'Settings', component: Settings }
 ];
 
 const router = createRouter({
