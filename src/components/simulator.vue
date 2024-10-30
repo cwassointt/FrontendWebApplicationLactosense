72a422bc07db4d203f1b32efb1a09eaeb7167e2f
cwassointt <dillpixel1@gmail.com>
2024-10-29 21:15:48 -0500

    feat: added structure for static component.

---
+++ src/components/simulator.vue
@@ -0,0 +1,25 @@
+<script>
+import Dashboard from "./dashboard.vue";
+import Alerts from "./alerts.vue";
+import Reports from "./reports.vue";
+import Settings from "./settings.vue";
+
+export default {
+  name: "simulator",
+  components: {Settings, Reports, Alerts, Dashboard},
+
+}
+</script>
+
+<template>
+  <div id="simulator">
+    <dashboard/>
+    <alerts/>
+    <reports/>
+    <settings/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
